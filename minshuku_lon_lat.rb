7ef58896c9cf72cb783cd2711943d8b1ceac6f90
Ayame Shirai <ayameshirai@ReasenoMacBook-Pro.local>
2023-05-12 12:43:32 +0900

    各ホテルの住所から緯度経度を取得

---
+++ minshuku_lon_lat.rb
@@ -0,0 +1,26 @@
+require 'csv'
+require 'httparty'
+
+csv_file = "yomikomi_minshuku.csv"
+CSV.foreach(csv_file) do |row|
+	address = row[3]
+
+	begin
+
+		response = HTTParty.get("https://msearch.gsi.go.jp/address-search/AddressSearch?q=#{ERB::Util.url_encode(address)}")
+		lonlat = response[0]["geometry"]["coordinates"]
+
+		CSV.open('scrape_minshuku_lon_lat.csv','a',:encoding => "utf-8") do |csv|
+			csv << [row[0], row[3], lonlat[0], lonlat[1]]
+		end
+
+	rescue
+
+		CSV.open('scrape_minshuku_lon_lat.csv','a',:encoding => "utf-8") do |csv|
+			csv << [row[0], row[3], "", ""]
+		end
+
+	end
+
+end
+
